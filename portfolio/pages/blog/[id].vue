4a903f8184c94fab5bec2ea78091087ea0c25cfb
Anton Bakhmutsky <anton_bakh@mail.ru>
2024-04-10 18:06:39 +0300

    clear

---
+++ portfolio/pages/blog/[id].vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    we are inside blog
+  </div>
+  <div>
+    Post id is {{ route.params.id  }}
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+</script>
